b540c47d897b6edcc80f64c0293d29f82ee67752
Walt Sorensen <photodude@users.noreply.github.com>
2016-09-29 10:42:36 -0600

    Update environments.php

--- Tests/environments.php
+++ Tests/environments.php
@@ -8,4 +8,8 @@
 $environments = array(
 	// The paths to Joomla cloned repo
 	'staging'     => realpath(__DIR__.'/environments/staging'),
+	'3.4'         => realpath(__DIR__.'/environments/3.4'),
+	'3.5-dev'     => realpath(__DIR__.'/environments/3.5-dev'),
+	'3.7.x'       => realpath(__DIR__.'/environments/3.7.x'),
+	'4.0-dev'     => realpath(__DIR__.'/environments/4.0-dev'),
 );
